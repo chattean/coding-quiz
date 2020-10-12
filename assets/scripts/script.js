14d30a72b95c81b5048832e6fd8d5bbcf05b54e1
Anuj Chatterjee <anujchatterjee@gmail.com>
2020-10-11 22:35:28 -0700

    adjusted the HTML, added stylesheet, setup the structure in HTML to be ready for JS

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,22 +1,43 @@
 // variable declarations
 var timerEl = document.getElementById('countdown'); //timer
+var mainEl = document.getElementById('main') // main div
 var questionsEl = document.getElementById('questions'); // put in content in the main
 var answersEl = document.getElementById('answers'); // put in content in the main
-var startBtn = document.getElementById('start');
+
+var startBtn = document.getElementById('start'); //start button
 
 
-var questionAndAnswerArray = [{q: "question 1", a: {choice1: "heel",validation:true },
-{q: "question 2", a: "yao"}]
+// creating answer form
+// answersEl.appendChild(option);
+// answersEl.appendChild(answersForm)
+// Question and answers array/objects
+var questionAndAnswerArray = 
+[{q: "question 1", a: [{choice: "heel",validation:false},{choice: "yell",validation:true}]},
+{q: "question 2", a: [{choice: "tell",validation:false},{choice: "mell",validation:true}]}
+]
+
 var score = 0;
-var timeLeft = 300;
+var timeLeft = 5;
+
 
 var quiz = function(){
     
-    for (var i = 0; i < questionAndAnswerArray.length; i++){
-        questionsEl.textContent = questionAndAnswerArray[i].q;
-        answersEl.textContent = questionAndAnswerArray[i].a;
-        
+    for (var i = 0; i <= questionAndAnswerArray.length-1; i++){
+        // var questionsEl = document.createElement("div")
+        questionsEl.innerHTML = questionAndAnswerArray[i].q;
+        console.log(questionAndAnswerArray[i].q)
+        // debugger;
+        for (var c = 0; c < questionAndAnswerArray[i].a.length;c++){
+            choice = questionAndAnswerArray[i].a[c].choice;
+            console.log(choice);
+            var choiceButton = document.createElement("BUTTON");
+            choiceButton.innerHTML = choice;
+            answersEl.appendChild(choiceButton)
+        }
 
+
+        // debugger;
+        // cleardiv();
         // var confirmAnswer = confirm(questionAndAnswerArray[i].q);
         // if (confirmAnswer === questionAndAnswerArray[i].a){
         //     alert("You are correct");
@@ -26,19 +47,27 @@ var quiz = function(){
         // }
     }
 }
+var cleardiv = function(){
+    questionsEl.innerHTML = ""
+    answersEl.innerHTML = ""
+
+
+}
 // alert("You got " + score + " correct");
 
 // Timer that counts down from 5
 function countdown() {  
+    quiz();
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function() {
       if (timeLeft>=1){
         timerEl.textContent = timeLeft;
         timeLeft--;
+        // quiz()
       }else{
         timerEl.textContent = "";
         clearInterval(timeInterval);
-        displayMessage()
+        // done();
       }
       
     }, 1000);
